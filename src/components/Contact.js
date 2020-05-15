4a101da8d2d9756434f7b8bf828533fd2f832786
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-15 19:08:28 +0530

    setTimeout redirect

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
-const Contact = () => {
+const Contact = (props) => {
+
+	// * programmatic redirect example
+	// * history.push redirects to '/about' after timeout
+	setTimeout(() => {
+		props.history.push('/about');
+	}, 2000);
+
 	return (
 		<div className="container">
 			<h4 className="center">Contact page</h4>
